656c617510919dbed9261284ddf44f9a259e5b12
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-09 10:23:37 -0300

    Update

--- test/br/com/cod3r/cm/CampoTeste.java
+++ test/br/com/cod3r/cm/CampoTeste.java
@@ -1,7 +1,8 @@
 package br.com.cod3r.cm;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.cod3r.cm.modelo.Campo;
@@ -10,11 +11,54 @@ public class CampoTeste {
 	
 	private Campo campo = new Campo(3, 3);
 	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3 , 3);
+	}
+	
 	@Test
-	void testeVizinhoRealDistancia1 () {
+	void testeVizinhoRealDistancia1Esquerda () {
 		Campo vizinho = new Campo(3 ,2);
+		boolean resultado  = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancia1Direita () {
+		Campo vizinho = new Campo(3 ,4);
 		boolean resultado  = campo.adicionarVizinho(vizinho);
 		assertTrue(resultado);
 		
 	}
+	
+	@Test
+	void testeVizinhoRealDistancia1Emcima () {
+		Campo vizinho = new Campo(2 ,3);
+		boolean resultado  = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+		
+	}
+	
+	@Test
+	void testeVizinhoRealDistancia1Embaixo () {
+		Campo vizinho = new Campo(4 ,3);
+		boolean resultado  = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancia2 () {
+		Campo vizinho = new Campo(2 ,2);
+		boolean resultado  = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNaoVizinho () {
+		Campo vizinho = new Campo(1 ,1);
+		boolean resultado  = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+		
+	}
+		
 }
