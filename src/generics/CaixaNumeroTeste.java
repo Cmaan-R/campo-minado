2a91f42cda8be8d2528c4661b25cee0d0a2981b3
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-12 10:42:51 -0300

    Update

--- src/generics/CaixaNumeroTeste.java
+++ src/generics/CaixaNumeroTeste.java
@@ -4,11 +4,11 @@ public class CaixaNumeroTeste {
 	
 	public static void main(String[] args) {
 		
-		CaixaNumero<Double> caixaA = new CaixaNumero();
+		CaixaNumero<Double> caixaA = new CaixaNumero<>();
 		caixaA.guardar(212.0);
 		System.out.println(caixaA.abrir());
 		
-		CaixaNumero<Integer> caixaB = new CaixaNumero();
+		CaixaNumero<Integer> caixaB = new CaixaNumero<>();
 		caixaB.guardar(212);
 		System.out.println(caixaB.abrir());
 	}
