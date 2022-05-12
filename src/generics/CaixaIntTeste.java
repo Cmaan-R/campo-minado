c9bf6303ffdba7892762f3e711a9522bbac71750
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-12 09:54:48 -0300

    Update

---
+++ src/generics/CaixaIntTeste.java
@@ -0,0 +1,17 @@
+package generics;
+
+public class CaixaIntTeste {
+	
+	public static void main(String[] args) {
+		
+		CaixaInt<String> caixaA = new CaixaInt<>();
+		caixaA.guardar("Teste");
+		
+		String coisaA = caixaA.abrir();
+		System.out.println(coisaA);
+		
+	
+	
+
+	}
+}
