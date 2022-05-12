7048b2ef4450a7a8e7ab8cf8f9a50b4d474eec75
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-12 10:05:38 -0300

    Update

---
+++ src/generics/CaixaNumeroTeste.java
@@ -0,0 +1,19 @@
+package generics;
+
+public class CaixaNumeroTeste {
+	
+	public static void main(String[] args) {
+		
+		CaixaNumero<Double> caixaA = new CaixaNumero();
+		caixaA.guardar(212.0);
+		System.out.println(caixaA.abrir());
+		
+		CaixaNumero<Integer> caixaB = new CaixaNumero();
+		caixaB.guardar(212);
+		System.out.println(caixaB.abrir());
+		
+		
+		
+		
+	}
+}
