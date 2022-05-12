b9170b168d950ee1350bd09c1cc858114dfc5dce
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-12 09:56:55 -0300

    Update

--- src/generics/CaixaIntTeste.java
+++ src/generics/CaixaIntTeste.java
@@ -4,14 +4,10 @@ public class CaixaIntTeste {
 	
 	public static void main(String[] args) {
 		
-		CaixaInt<String> caixaA = new CaixaInt<>();
-		caixaA.guardar("Teste");
+		CaixaInt caixaA = new CaixaInt();
+		caixaA.guardar(123);
 		
-		String coisaA = caixaA.abrir();
+		Integer coisaA = caixaA.abrir();
 		System.out.println(coisaA);
-		
-	
-	
-
 	}
 }
