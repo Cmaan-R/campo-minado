61be85d8c9adbbdf69a3e9565fd99f2811fdd18f
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-18 11:11:14 -0300

    Update

--- src/padroes/observer/AniversarioSurpresa.java
+++ src/padroes/observer/AniversarioSurpresa.java
@@ -3,10 +3,13 @@ package padroes.observer;
 public class AniversarioSurpresa {
 	
 	public static void main(String[] args) {
-		Namorada namorada = new Namorada();
+		// Namorada namorada = new Namorada();
 		Porteiro porteiro = new Porteiro();
 		
-		porteiro.registrarObservador(namorada);
+		//porteiro.registrarObservador(namorada);
+		
+		porteiro.registrarObservador(e 
+				-> System.out.println("Surpresa via Lambda!"));
 		porteiro.monitorar();
 	
 	}
